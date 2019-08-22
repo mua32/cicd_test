4126b9f58e3319006447d475783356d92375336c
System Administrator <admin@example.com>
2019-08-22 18:20:36 +0530

    Changes by 'System Administrator' on 'CICD Test – 1.0.0' -- Build #1

---
+++ sn/CICD Test/Fix Script/active_true/Testing Global Scoped .js
@@ -0,0 +1,10 @@
+/* 
+ * Application : CICD Test
+ * ClassName   : sys_script_fix
+ * Created On  : 2019-08-22 12:43:23
+ * Created By  : admin
+ * Updated On  : 2019-08-22 12:43:23
+ * Updated By  : admin
+ * URL         : /sys_script_fix.do?sys_id=0e9eda67db13f300e76e771c8c9619cf
+ */
+gs.log("Hey");
